6daea08d15e56ed6bb0d5810bf7d5e5c9ff15791
pyzzd <7698346+pyzzd@user.noreply.gitee.com>
2023-06-15 22:45:18 +0800

    createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import Index from '@/components/Index.vue'
 import Mobile from "@/components/Mobile.vue";
 
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {path: '/', name: "Index", component: Index},
         {path: '/mobile', name: "Mobile", component: Mobile}
