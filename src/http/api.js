1ebb38d24204e82b28af072a90ec8820a0cfc84a
484256249@qq.com <dj123456>
2023-09-04 23:31:57 +0800

    接口加密

--- src/http/api.js
+++ src/http/api.js
@@ -1,8 +1,34 @@
 import service from "@/http/request"
 
+
+function b64encrypt(plaintext) {
+    const b64Text = btoa(plaintext)
+    let result = ""
+    for (const char of b64Text) {
+        result += (btoa(char)).slice(0, 2);
+    }
+    return result
+}
+
+function b64decrypt(inputString) {
+    let splitString = [];
+    for (let i = 0; i < inputString.length; i += 2) {
+        const chunk = inputString.slice(i, i + 2);
+        splitString.push(atob(chunk + "=="));
+    }
+    splitString = splitString.join("")
+    console.log(atob(splitString))
+}
+
 export function heroData() {
+    const plaintext = "https://daluandou.cn/" + "@" + new Date().getTime().toString();
+    let result = b64encrypt(plaintext)
     return service({
-        method: "GET",
-        url: "api/lol/heroes"
+        method: "POST",
+        url: "api/lol/heroes",
+        data: {"dld": result},
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
     })
 }
